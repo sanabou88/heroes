bcc0f3438544e6e8b76bb1ed32cf600499ada14e
sanabou88 <bouazizsana363@gmail.com>
2024-01-22 17:09:48 +0100

    my application completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
 import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './app-routing.module';
 
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+import { RouterModule } from '@angular/router';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 @NgModule({
     declarations: [
         AppComponent,
+        DashboardComponent,
+        HeroesComponent,
+        HeroSearchComponent
+        
     ],
-    providers: [],
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
+        RouterModule,
         FormsModule,
-        HeroesComponent,
-        HeroDetailComponent,
+        AppRoutingModule,
         MessagesComponent,
-        AppRoutingModule
-    ]
+        HeroDetailComponent,
+        HttpClientModule,
+        HttpClientInMemoryWebApiModule.forRoot(
+        InMemoryDataService, { dataEncapsulation: false }
+          )
+
+    ],
 })
 export class AppModule { }
